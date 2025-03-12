6c83f8fa926724d10f7e3c383f25ba24d2a7311f
Edward Song <111555193+edwardsong08@users.noreply.github.com>
2025-03-11 23:50:08 -0400

    vzw-transaction-ledger project initial setup

---
+++ src/main/java/com/example/vzw_transaction_ledger/VzwTransactionLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.example.vzw_transaction_ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VzwTransactionLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VzwTransactionLedgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vzw_transaction_ledger/VzwTransactionLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vzw_transaction_ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VzwTransactionLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
